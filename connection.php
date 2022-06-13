06bfa16b342e44770917429808db713adbf02a63
Mutesa-Cedric <mutesacedric@gmail.com>
2022-06-14 00:17:03 +0200

    update : register users and adminstrator

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+//database connection oop
+$connection=new mysqli("localhost","root","","store_ms");
+
+if($connection->connect_error){
+    die("connection failed: ".$connection->connect_error);
+}else{
+    echo " database \"store_ms\" was connected successfully";
+}
+//database connection oop
+
+?>
